72bb86fc9587c8c35b6208f152f66b14ada6908e
Fritz Larco <flarco@live.com>
2026-01-05 15:01:15 -0300

    abstract platform-specific library loading
    - introduce `loadLibrary` function for platform-specific library loading
    - use `purego.Dlopen` for Unix-like systems (Linux, macOS)
    - use `syscall.LoadLibrary` for Windows
    - decouple `purego` import to platform-specific files

--- go/adbc/drivermgr/library.go
+++ go/adbc/drivermgr/library.go
@@ -22,8 +22,6 @@ import (
 	"os"
 	"runtime"
 	"sync"
-
-	"github.com/ebitengine/purego"
 )
 
 var (
@@ -56,7 +54,7 @@ func loadDriverManagerLibrary() (uintptr, error) {
 		}
 
 		var lib uintptr
-		lib, loadErr = purego.Dlopen(libName, purego.RTLD_NOW|purego.RTLD_GLOBAL)
+		lib, loadErr = loadLibrary(libName)
 		if loadErr != nil {
 			loadErr = fmt.Errorf("failed to load ADBC driver manager library %q: %w. You can specify the exact path with env var ADBC_DRIVER_MANAGER_LIB", libName, loadErr)
 			return
