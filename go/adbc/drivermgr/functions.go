81c794eeb174f657df4b4d524fce36afcc24c1ce
Fritz Larco <flarco@live.com>
2026-01-05 10:53:58 -0300

    implement shared library loading
    - add `loadDriverManagerLibrary` to dynamically load the ADBC driver manager shared library
    - utilize `purego` for platform-specific library loading (darwin, linux, windows)
    - allow overriding library path via `ADBC_DRIVER_MANAGER_LIB` environment variable
    - introduce `registerFunctions` as a placeholder for future ADBC function bindings

---
+++ go/adbc/drivermgr/functions.go
@@ -0,0 +1,26 @@
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+package drivermgr
+
+// registerFunctions registers all ADBC function bindings with purego.
+// This is called once during library loading.
+// Full implementation will be added in Phase 3.
+func registerFunctions(lib uintptr) error {
+	// TODO: Phase 3 - Register all function bindings
+	return nil
+}
